2cbcbac6877227d54316acd7ad421a68dbee6db8
rolandgulyas90 <rolandgulyas90@gmail.com>
2025-09-29 22:16:52 +0200

    test: előre/hátra mozgás

--- test_main.py
+++ test_main.py
@@ -15,7 +15,21 @@ def test_turning_left_and_right():
     b.turn_right(); assert b.direction == Direction.W
     b.turn_right(); assert b.direction == Direction.N
 
-    b.turn_left(); assert b.direction == Direction.E
-    b.turn_left(); assert b.direction == Direction.S
     b.turn_left(); assert b.direction == Direction.W
+    b.turn_left(); assert b.direction == Direction.S
+    b.turn_left(); assert b.direction == Direction.E
     b.turn_left(); assert b.direction == Direction.N
+
+def test_forward_and_backward():
+    planet = Moon(10, 10, [])
+    b = Buggy(planet, Position(3,3), Direction.N)
+
+    b.move_forward()
+    assert b.position == Position(3,4)
+
+    b.turn_right()
+    b.move_forward()
+    assert b.position == Position(4,4)
+
+    b.move_backward()
+    assert b.position == Position(3,4)
