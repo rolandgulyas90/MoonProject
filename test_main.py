ecde303cf431ef8bf9efa8ec7b5a53e861688334
rolandgulyas90 <rolandgulyas90@gmail.com>
2025-09-29 22:04:11 +0200

    test: fordulások balra és jobbra körbeforgás ellenőrzéssel

--- test_main.py
+++ test_main.py
@@ -1,7 +1,21 @@
 from main import Buggy, Moon, Direction, Position
 
 def test_initial_state():
-    planet = Planet(width = 5, height  5, obstacles=[])
+    planet = Moon(width = 5, height = 5, obstacles=[])
     b = Buggy(planet=planet, position=Position(2, 3),direction=Direction.N)
     assert b.position == Position(2, 3)
     assert b.direction == Direction.N
+
+def test_turning_left_and_right():
+    planet = Moon(5, 5, [])
+    b = Buggy(planet, Position(0,0), Direction.N)
+
+    b.turn_right(); assert b.direction == Direction.E
+    b.turn_right(); assert b.direction == Direction.S
+    b.turn_right(); assert b.direction == Direction.W
+    b.turn_right(); assert b.direction == Direction.N
+
+    b.turn_left(); assert b.direction == Direction.E
+    b.turn_left(); assert b.direction == Direction.S
+    b.turn_left(); assert b.direction == Direction.W
+    b.turn_left(); assert b.direction == Direction.N
